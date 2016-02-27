36fb3f212e8aee1bc6445d8f4b0a000e54610597
José Carlos Fernández <jc.fernandez@me.com>
2016-02-27 09:26:44 -0600

    Prueba para quitar comportamiento aleatorio

--- javascript/2050_main_BRYAN.js
+++ javascript/2050_main_BRYAN.js
@@ -37,7 +37,8 @@
 				strDebug += ctrName + " = " + value + "\n";
 			}
 
-			var RandVal = Math.floor(Math.random() * 100);
+			//var RandVal = Math.floor(Math.random() * 100);
+			var RandVal = 3*100
 			var newVal = RandVal;
 
 			$.post(urlModel, {
